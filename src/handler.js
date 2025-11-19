0e97b24bab09806ead520699461fd81730169b8d
Red_thz <subhan.qasimi34@gmail.com>
2025-11-19 19:47:11 +0100

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,11 @@
 export default function handler(req, res) {
     console.log("✔️ Interaction received:", req.body);
 
-    // Discord PING
     if (req.body?.type === 1) {
         console.log("✔️ Responding to PING");
         return res.json({ type: 1 });
     }
 
-    // Für alle Commands – test response
     return res.json({
         type: 4,
         data: {
