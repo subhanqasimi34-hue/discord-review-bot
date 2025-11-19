e42ca763d6f460b55aa7b40503a0a02f0166787e
Red_thz <subhan.qasimi34@gmail.com>
2025-11-19 20:53:04 +0100

    Delete src directory

--- src/utils/xpSystem.js
+++
@@ -1,8 +0,0 @@
-function getXPBar(xp, needed, size = 20) {
-    const filled = Math.round((xp / needed) * size);
-    const empty = size - filled;
-
-    return "▰".repeat(filled) + "▱".repeat(empty);
-}
-
-module.exports = { getXPBar };
