dd8b59915150bb22430f15db5ea084ca2bb546db
Red_thz <subhan.qasimi34@gmail.com>
2025-11-19 22:09:51 +0100

    Delete src directory

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-export const data = {
-    name: "ping",
-    description: "Replies with Pong!"
-};
-
-export async function execute(interaction) {
-    await interaction.reply("Pong!");
-}
--- src/utlis/progressBar.js
+++
@@ -1,4 +0,0 @@
-export function createProgressBar(current, max, size = 20) {
-    const filled = Math.round((current / max) * size);
-    return "█".repeat(filled) + "░".repeat(size - filled);
-}
